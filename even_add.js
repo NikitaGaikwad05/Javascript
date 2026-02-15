58266c4f254401d996ebd087dcf7d82660830c42
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-15 23:13:30 +0530

    Add number sign check in even_add.js

---
+++ even_add.js
@@ -0,0 +1,11 @@
+// to check the number is positive,negative,equal to zero
+var num=23;
+if(num===0){
+    console.log("number is zero");
+}
+else if(num>0){
+    console.log("number positive");
+}
+else{
+    console.log("number negative");
+}
