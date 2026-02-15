b31de46595e7de8d0f2c51b81507fcfe64d7faa9
GaikwadNikita <gaikwadnikitamahadev1@gmail.com>
2026-02-15 23:22:58 +0530

    Add switch case for day of the week

---
+++ menu.js
@@ -0,0 +1,15 @@
+//switch case
+var day="sunday";
+switch(day){
+    case "monday":
+        console.log("today is monday");
+        break;
+    case "friday":
+        console.log("omg lets have  party day");
+        break;
+    case "sunday":
+        console.log(" movie day");
+        break;
+    default:
+    console.log("no condition match");
+}
